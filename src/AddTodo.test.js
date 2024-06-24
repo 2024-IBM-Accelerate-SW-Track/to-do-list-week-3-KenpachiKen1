ecf603f9340cc3036e315483372a3e083285ebd6
Ken <kentimi35@gmail.com>
2024-06-24 19:10:48 -0400

    Lab week 3 numba 5

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent} from '@testing-library/react';
+import { render, screen as screen, fireEvent} from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 import '@testing-library/jest-dom/extend-expect';
@@ -20,9 +20,8 @@ afterEach(() => {
 
 
 
- test('test that App component doesn\'t render dupicate Task', () => {
+test('App component doesn\'t render duplicate Task', () => {
   render(<App />);
-
   const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
   const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
@@ -37,14 +36,11 @@ afterEach(() => {
   fireEvent.change(inputDate, {target: {value: dueDate}});
   fireEvent.click(element);
 
-
   const tasks = screen.getAllByText(new RegExp(taskName, "i"));
   expect(tasks.length).toBe(1);
+});
 
-
- });
-
- test('test that App component doesn\'t add a task without task name', () => {
+test('App component doesn\'t add a task without task name', () => {
   render(<App />);
   const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
@@ -54,31 +50,22 @@ afterEach(() => {
   fireEvent.click(element);
   const tasks = screen.queryByText(/06\/15\/2023/i);
   expect(tasks).not.toBeInTheDocument();
+});
 
- });
-
- test('test that App component doesn\'t add a task without due date', () => {
+test('App component doesn\'t add a task without due date', () => {
   render(<App />);
-
   const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
   const element = screen.getByRole('button', {name: /Add/i});
-
   const taskName = "Science Test";
 
-  
   fireEvent.change(inputTask, {target: {value: taskName}});
   fireEvent.click(element);
   const tasks = screen.queryByText(new RegExp(taskName, "i"));
   expect(tasks).not.toBeInTheDocument();
+});
 
- });
-
-
-
- test('test that App component can be deleted thru checkbox', () => {
+test('App component can be deleted through checkbox', () => {
   render(<App />);
-
-  
   const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
   const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
@@ -89,18 +76,15 @@ afterEach(() => {
   fireEvent.change(inputDate, {target: {value: dueDate}});
   fireEvent.click(element);
 
+  const checkbox = screen.getByRole('checkbox');
+  fireEvent.click(checkbox);
 
-  const card = screen.getByTestId(/Math Test/i).style.background
-  expect(card).toHaveStyle('background-color: red');
+  const tasks = screen.queryByText(new RegExp(taskName, "i"));
+  expect(tasks).not.toBeInTheDocument();
+});
 
-  
-
- });
-
-
- test('test that App component renders different colors for past due events', () => {
+test('App component renders different colors for past due events', () => {
   render(<App />);
-
   const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
   const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
@@ -111,9 +95,6 @@ afterEach(() => {
   fireEvent.change(inputDate, {target: {value: dueDate}});
   fireEvent.click(element);
 
-  const checkbox = screen.getByRole('Checkbox');
-  fireEvent.click(checkbox);
-
-  const tasks= screen.queryByText(new RegExp(taskName, "i"));
-  expect(tasks).not.toBeInTheDocument();
- });
+  const card = screen.getByTestId(taskName);
+  expect(card).toHaveStyle('background-color: red');  // Assuming red color is used for past due events
+});
